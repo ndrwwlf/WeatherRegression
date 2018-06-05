4bcd41a9b81b5255844e0c043ebf5bbf43530347
ewolfmsla <ewolf49@gmail.com>
2018-06-04 21:15:16 -0600

    trim to rtrim

---
+++ WeatherService/Repository/IWeatherRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using WeatherService.Db;
+using WeatherService.Dto;
+
+namespace WeatherService.Services
+{
+    public interface IWeatherRepository
+    {
+        List<Location> GetLocations();
+        Location GetLocation(int id);
+        Location InsertLocation(PutPostLocation location);
+        bool DeleteLocation(int id);
+        bool UpdateLocation(Location location);
+    }
+}
